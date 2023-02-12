9d522ba45746531ecbee622f07431c1073f26247
JeongTaekCho <cjt3591@gmail.com>
2023-02-12 15:20:08 +0900

    ✨ Feat :  place-routes 컨트롤러 추가

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,47 +1,10 @@
 const express = require("express");
-const HttpError = require("../models/http-error");
+const placeControllers = require("../controllers/place-controllers");
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "엠파이어 스테이트 빌딩",
-    description: "세상에서 가장 높은 빌딩임",
-    location: {
-      lat: 40.7484474,
-      lng: -73.9871516,
-    },
-    address: "20 W 34th st, new york, NY 10001",
-    creator: "u1",
-  },
-];
+router.get("/:pid", placeControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
-
-  if (!place) {
-    throw new HttpError("could not find a place for the provided id.", 404);
-  }
-
-  res.json({ place });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-
-  if (!place) {
-    return next(
-      new HttpError("could not find a place for the provided id.", 404)
-    );
-  }
-  res.json({ place });
-});
+router.get("/user/:uid", placeControllers.getPlaceByUserId);
 
 module.exports = router;
