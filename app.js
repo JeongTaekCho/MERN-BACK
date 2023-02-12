039eb28f567f158115b491795e9a371b8b8d9929
JeongTaekCho <cjt3591@gmail.com>
2023-02-12 16:39:46 +0900

    ✨ Feat :  지원하지 않는 라우트 오류 로직 추가

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  const error = new HttpError("could not find this route", 404);
+  throw error;
+});
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
