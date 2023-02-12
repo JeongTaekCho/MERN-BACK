658f4e72665fe64327bbbdeab02dfdcf0fa1bb83
JeongTaekCho <cjt3591@gmail.com>
2023-02-12 16:35:25 +0900

    ✨ Feat :  createPlace 라우트 추가 / uuid 모듈 추가

--- controllers/place-controllers.js
+++ controllers/place-controllers.js
@@ -1,4 +1,5 @@
 const HttpError = require("../models/http-error");
+const { v4 } = require("uuid");
 
 const DUMMY_PLACES = [
   {
@@ -41,5 +42,23 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+
+  const createPlace = {
+    id: v4(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  DUMMY_PLACES.push(createPlace);
+
+  res.status(201).json({ place: createPlace });
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
