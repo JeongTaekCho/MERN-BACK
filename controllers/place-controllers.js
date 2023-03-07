6b52e94200ccf13d52108adf717b8303cb172f23
JeongTaekCho <cjt3591@gmail.com>
2023-03-07 19:48:10 +0900

    ✨ Feat :  장소얻기 - 대안

--- controllers/place-controllers.js
+++ controllers/place-controllers.js
@@ -55,16 +55,17 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  let places;
+  // let places;
+  let userWithPlaces;
 
   try {
-    places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate("places");
   } catch (err) {
     const error = new HttpError("장소 가져오기에 실패 했습니다.", 500);
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     const error = new HttpError(
       "could not find a places for the provided id.",
       404
@@ -72,7 +73,9 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
   res.json({
-    places: places.map((place) => place.toObject({ getters: true })),
+    places: userWithPlaces.places.map((place) =>
+      place.toObject({ getters: true })
+    ),
   });
 };
 
